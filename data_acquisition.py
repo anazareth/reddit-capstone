18bb4b676f4a2628fd2ba6940fbfc6e37abac74c
Alex Nazareth <anazareth@ryerson.ca>
2018-07-11 23:31:16 -0400

    get /r/upliftingnews

--- data_acquisition.py
+++ data_acquisition.py
@@ -10,30 +10,26 @@ api = PushshiftAPI()
 SEARCH_LIMIT = 1000
 MAX_ROWS = 1048000  # approx excel lim (for convenience)
 MIN_COMMENTS = 1
-start_epoch = int(dt.datetime(2016, 8, 2).timestamp())
-END_EPOCH = int(dt.datetime(2017, 8, 2).timestamp())
+OUT_CSV_NAME = "reddit_upliftingnews_2016_all.csv"
+SUBREDDIT = "upliftingnews"
+start_epoch = int(dt.datetime(2016, 1, 1).timestamp())
+END_EPOCH = int(dt.datetime(2016, 12, 31).timestamp())
 print(time.strftime("%a %b %d %H:%M:%S %Z") + " -- Starting program...")
 
-raw_fields = ['url','author', 'title', 'subreddit', 'created_utc', 'permalink', 'score', 'id', 'num_comments',
-              'is_self', 'is_video', 'num_crossposts', 'over_18', 'whitelist_status', 'domain',
-              'author_flair_text', 'author_flair_type', 'locked', 'media_only', 'pinned','body']
+raw_fields = ['url','author', 'title', 'subreddit', 'created_utc', 'permalink', 'score', 'id', 'num_comments','domain', 'locked']
 
 submissions = pd.DataFrame()
 
-start_search = api.search_submissions(after=start_epoch, subreddit='news', filter=raw_fields, limit=SEARCH_LIMIT)
+start_search = api.search_submissions(after=start_epoch, subreddit=SUBREDDIT, filter=raw_fields, limit=SEARCH_LIMIT)
 submissions = pd.DataFrame.from_dict(start_search) #.to_frame()
 submissions = submissions["d_"].apply(pd.Series).dropna(subset=["created"])
 start_epoch = int(submissions.loc[len(submissions)-1, "created"] + 1)
 submissions = submissions[submissions["num_comments"]>=MIN_COMMENTS]
 
-#116023
-#missing:
-#1488334783
-#1488347494
-
+# TODO: create functions with date range as input (maybe try to fill missing data)
 
 while(True):
-    search_results = api.search_submissions(after=start_epoch, subreddit='news', filter=raw_fields, limit=SEARCH_LIMIT)
+    search_results = api.search_submissions(after=start_epoch, subreddit=SUBREDDIT, filter=raw_fields, limit=SEARCH_LIMIT)
     temp_submissions = pd.DataFrame.from_dict(search_results).loc[:,"d_"].to_frame()
     temp_submissions = temp_submissions["d_"].apply(pd.Series)
     start_epoch = int(temp_submissions.loc[len(temp_submissions)-1, "created"] + 1)
@@ -45,8 +41,7 @@ while(True):
     print(start_epoch)
     print(len(submissions))  # to see progress
 
-submissions.to_csv("reddit_news_20160802_1year_comments_utf8.csv", encoding = "utf-8")
-submissions.to_csv("reddit_news_20160802_1year_comments.csv")
+submissions.to_csv(OUT_CSV_NAME)
 
 print(time.strftime("%a %b %d %H:%M:%S %Z"),
       " -- Done, elapsed time: ", str(time.time() - start_time))
